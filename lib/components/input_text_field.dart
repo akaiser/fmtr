2665905612170b5a1bd48ef1ffd551b8da051010
Albert Kaiser <ak@kaiserv.de>
2025-11-07 22:59:55 +0100

    v1.0.0 (#3)

---
+++ lib/components/input_text_field.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class InputTextField extends StatelessWidget {
+  const InputTextField({
+    required this.valueNotifier,
+    required this.errorNotifier,
+  });
+
+  final ValueNotifier<String> valueNotifier;
+  final ValueNotifier<String?> errorNotifier;
+
+  @override
+  Widget build(BuildContext context) => ValueListenableBuilder(
+    valueListenable: errorNotifier,
+    builder: (context, errorText, counter) => TextField(
+      autofocus: true,
+      maxLines: null,
+      onChanged: (text) => valueNotifier.value = text,
+      decoration: InputDecoration(
+        errorText: errorText,
+        border: const OutlineInputBorder(),
+        labelText: 'In',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        counter: counter,
+      ),
+    ),
+    child: ValueListenableBuilder(
+      valueListenable: valueNotifier,
+      builder: (_, valueText, _) => Text('${valueText.length}'),
+    ),
+  );
+}
---
+++ test/_version_test.dart
@@ -0,0 +1,8 @@
+import 'package:fmtr/_version.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('has expected packageVersion', () {
+    expect(packageVersion, '1.0.0');
+  });
+}
