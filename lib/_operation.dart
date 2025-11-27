c9f475675f79f3b454886ae2f5ea9d6a4078f61a
Albert Kaiser <ak@kaiserv.de>
2025-11-27 23:56:32 +0100

    json minify

--- lib/_operation.dart
+++ lib/_operation.dart
@@ -1,6 +1,8 @@
 enum Operation {
-  normalization._('Normalization'),
-  prettyJson._('Pretty JSON'),
+  list._('List'),
+  json._('JSON'), // Prettify, Minify
+  // base64._('Base64'), // Encode, Decode, URL-safe
+  // conversion._('Conversion'), // JSON > YAML, YAML > JSON
   ;
 
   const Operation._(this.label);
--- lib/_option.dart
+++ lib/_option.dart
@@ -1,4 +1,5 @@
 enum Option {
+  // List
   standardizeSpacing._('Standardize spacing'),
   sortAlphabetically._('Sort alphabetically'),
   reverseOrder._('Reverse order'),
@@ -6,6 +7,10 @@ enum Option {
   lowercase._('Lowercase'),
   uppercase._('Uppercase'),
   removeDuplicates._('Remove duplicates'),
+
+  // JSON
+  prettify._('Prettify'),
+  minify._('Minify'),
   ;
 
   const Option._(this.label);
@@ -14,6 +19,7 @@ enum Option {
 }
 
 extension OptionExt on Option {
+  // List
   bool get isIgnoreCase => this == Option.ignoreCase;
 
   bool get isLowercase => this == Option.lowercase;
@@ -21,28 +27,37 @@ extension OptionExt on Option {
   bool get isUppercase => this == Option.uppercase;
 
   bool get isRemoveDuplicates => this == Option.removeDuplicates;
+
+  // JSON
+  bool get isPrettify => this == Option.prettify;
+
+  bool get isMinify => this == Option.minify;
 }
 
 extension OptionsMapExt on Map<Option, bool> {
-  bool _enabled(Option option) => this[option] ?? false;
+  bool _isTrue(Option option) => this[option] ?? false;
 
-  bool get hasEnabledStandardizeSpacing => _enabled(Option.standardizeSpacing);
+  // List
+  bool get hasEnabledStandardizeSpacing => _isTrue(Option.standardizeSpacing);
 
-  bool get hasEnabledSortAlphabetically => _enabled(Option.sortAlphabetically);
+  bool get hasEnabledSortAlphabetically => _isTrue(Option.sortAlphabetically);
 
-  bool get hasEnabledReverseOrder => _enabled(Option.reverseOrder);
+  bool get hasEnabledReverseOrder => _isTrue(Option.reverseOrder);
 
-  bool get hasEnabledIgnoreCase => _enabled(Option.ignoreCase);
+  bool get hasEnabledIgnoreCase => _isTrue(Option.ignoreCase);
 
-  bool get hasEnabledLowercase => _enabled(Option.lowercase);
+  bool get hasEnabledLowercase => _isTrue(Option.lowercase);
 
-  bool get hasEnabledUppercase => _enabled(Option.uppercase);
+  bool get hasEnabledUppercase => _isTrue(Option.uppercase);
 
-  bool get hasEnabledRemoveDuplicates => _enabled(Option.removeDuplicates);
+  bool get hasEnabledRemoveDuplicates => _isTrue(Option.removeDuplicates);
+
+  // JSON
+  bool get hasEnabledPrettify => _isTrue(Option.prettify);
+
+  bool get hasEnabledMinify => _isTrue(Option.minify);
 
   // misc
-
-  bool get hasInvalidCaseOptions => hasEnabledLowercase && hasEnabledUppercase;
 
   bool get ignoreCaseMaybeEnabled =>
       hasEnabledSortAlphabetically &&
